68086b7e9071b3723d6548b9bc2e4754791f58a8
ambroz283878 <283878@student.pwr.edu.pl>
2026-06-19 00:03:28 +0200

    TopGuy actions

--- main.py
+++ main.py
@@ -13,9 +13,14 @@ help_str="""
     view <option>:
         -list: list all available views
         -show <view_id>:show contents of selected view
+    table <option>:
+        -list: list all available tables
+        -show <table_id>:show contents of selected table
 """
 
 load_dotenv()
+
+permissions={"Guy":1,"TopGuy":3,"BigYahoo":5}
 
 DB_URL=os.getenv("DB_URL")
 
@@ -25,6 +30,7 @@ if not DB_URL:
 
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
+    print("Enter command. Type 'help' for help")
 
 def mainLoop(permLvl):
     command = input("\nTUI: ").strip().split()
@@ -45,6 +51,41 @@ def mainLoop(permLvl):
             except IndexError:
                 print("Must provide a valid option")
                 return
+        case "table":
+            if permLvl < permissions["TopGuy"]:
+                print("You do not have permissions to use this command")
+                return
+            try:
+                option=command[1]
+                if option=="show":
+                    table_id=command[2]
+                    if not table_id.replace('_', '').replace('.', '').isalnum():
+                        print("    BŁĄD: Nieprawidłowa nazwa tabeli!")
+                        return []
+                    print(executeQuery(f"""SELECT * FROM {table_id}""", ()))
+                elif option=="list":
+                    print(executeQuery(queries.listTables, ()))
+            except IndexError:
+                print("Must provide a valid option")
+                return
+        case "set-status":
+            if permLvl < permissions["TopGuy"]:
+                print("You do not have permissions to use this command")
+                return
+
+            try:
+                status=command[1]
+                user_id=command[2]
+            except IndexError:
+                print("Must provide a status and user_id")
+                return
+            
+            if status not in validStatuses:
+                print("Must provide a valid status:" \
+                "ACTIVE, DELETED, SUSPENDED, WATCHED")
+                return
+            executeQuery(queries.changeUserStatus,(status,user_id))
+
         case "exit":
             exit()
         case "clear":
@@ -66,7 +107,12 @@ def executeQuery(sqlQuery: str, param: tuple)->list:
     with oracledb.connect(dsn=DB_URL) as DB_CONN:
         with DB_CONN.cursor() as cur:
             cur.execute(sqlQuery, param)
-            rows = cur.fetchall()
+            DB_CONN.commit()
+            try:
+                rows = cur.fetchall()
+            except Exception as e:
+                print(e)
+                return []
     return rows
 
 def login():
@@ -75,14 +121,14 @@ def login():
     credentials = (input("Enter login:\n ->"),sha256(getpass("Enter password:\n ->").encode()).hexdigest())
     return executeQuery(queries.login,credentials)
 
-permLevel = login()
+permLevel = login()[0][0]
 cls()
 if permLevel:
-    print("\n","="*29,f"\n  Login successful\n  Permission level: {permLevel[0][0]}","\n","="*29)
+    print("\n","="*29,f"\n  Login successful\n  Permission level: {permLevel}","\n","="*29)
 else:
     print("Invalid credentials")
     exit
-sleep(3)
-#cls()    
+#cls()
+validStatuses=[i[0] for i in executeQuery(queries.listStatuses,())]
 while True:
     mainLoop(permLevel)
