7298b6e0ffbaf20ad839fb06101671e89359c195
ambroz283878 <283878@student.pwr.edu.pl>
2026-06-18 23:18:43 +0200

    Added main loop for level 1 permissions

--- main.py
+++ main.py
@@ -6,30 +6,64 @@ import os
 import oracledb
 import queries
 
+help_str="""
+    clear: clear TUI
+    exit: exit program
+    help: show this list
+    view <option>:
+        -list: list all available views
+        -show <view_id>:show contents of selected view
+"""
+
 load_dotenv()
 
 DB_URL=os.getenv("DB_URL")
 
 if not DB_URL:
     raise ValueError("Database URL is missing")
-DB_CONN=oracledb.connect(dsn=DB_URL)
+#DB_CONN=oracledb.connect(dsn=DB_URL)
 
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 
-def mainLoop():
-    cls()
-    command = input("\nTUI: ")
-    match command:
+def mainLoop(permLvl):
+    command = input("\nTUI: ").strip().split()
+    if not command:
+        return
+    match command[0]:
+        case "view":
+            try:
+                option=command[1]
+                if option=="show":
+                    view_id=command[2]
+                    if not view_id.replace('_', '').replace('.', '').isalnum():
+                        print("    BŁĄD: Nieprawidłowa nazwa widoku!")
+                        return []
+                    print(executeQuery(f"""SELECT * FROM {view_id}""", ()))
+                elif option=="list":
+                    print(executeQuery(queries.listViews, ()))
+            except IndexError:
+                print("Must provide a valid option")
+                return
+        case "exit":
+            exit()
+        case "clear":
+            cls()
+        case "help":
+            print("")
         case _:
-            print(f"Unknown command '{command}'")
-    input()
+            print(f"Unknown command '{command}' - see 'help'")
 
 def executeQuery(sqlQuery: str, param: tuple)->list:
     if not sqlQuery:
         print("    WARNING! executeQuery: No query provided!")
         return None
-    with DB_CONN:
+    if not param:
+        with oracledb.connect(dsn=DB_URL) as DB_CONN:
+            with DB_CONN.cursor() as cur:
+                cur.execute(sqlQuery)
+                rows = cur.fetchall()
+    with oracledb.connect(dsn=DB_URL) as DB_CONN:
         with DB_CONN.cursor() as cur:
             cur.execute(sqlQuery, param)
             rows = cur.fetchall()
@@ -44,8 +78,11 @@ def login():
 permLevel = login()
 cls()
 if permLevel:
-    print("\n","="*29,f"    Login successful\n  Permission level: {permLevel[0][0]}","\n","="*29)
+    print("\n","="*29,f"\n  Login successful\n  Permission level: {permLevel[0][0]}","\n","="*29)
 else:
     print("Invalid credentials")
-sleep(5)
+    exit
+sleep(3)
 #cls()    
+while True:
+    mainLoop(permLevel)
